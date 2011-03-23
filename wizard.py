c2d70d408f85f2cfe35aa566b1fed93119b0f80e
Thibaut DIRLIK <thibaut.dirlik@gmail.com>
2011-03-23 16:33:01 +0100

    Invoice generation is almost working.

--- wizard.py
+++ wizard.py
@@ -18,6 +18,7 @@
 #
 
 import datetime
+import math
 
 from osv import osv, fields
 from tools.translate import _
@@ -47,31 +48,65 @@ class CreateCommissionsInvoice(osv.osv_memory):
 
         suppliers_commissions = {}
         suppliers_invoices = {}
+
+        try:
+            product_id = self.pool.get('ir.model.data').read(cursor, user_id,
+            self.pool.get('ir.model.data').search(cursor, user_id,
+                [('module', '=', 'commissions'),('name', '=', 'commission_product')],context=context
+            ), context=context)[0]['res_id']
+            product = self.pool.get('product.product').browse(cursor, user_id, product_id, context=context)
+        except KeyError, e:
+            raise osv.except_osv(_('Error'), _('You removed the Commission product. Update the module.'))
         
         for commission in commissions:
             suppliers_commissions.setdefault(commission.supplier_id, list())
             suppliers_commissions[commission.supplier_id].append(commission)
 
         for supplier, commissions in suppliers_commissions.iteritems():
-            # For each supplier, we generate an invoice with the specified commissions.
-            # We also write the the invoice_line_id to commissions objects.
+            # Generate invoice
             invoice_id = self.pool.get('account.invoice').create(cursor, user_id, {
                 'type' : 'out_invoice',
                 'state' : 'draft',
                 'date_invoice' : datetime.date.today(),
                 'partner_id' : supplier.id,
-                'address_invoice_id' : commissions.order_id.partner_invoice_id.id,
+                'address_invoice_id' : commissions[0].order_id.partner_invoice_id.id,
                 'account_id' : supplier.property_account_receivable.id,
             }, context=context)
             suppliers_invoices[supplier] = invoice_id
+
+            # Generate invoice line - One customer / line
+            invoice_lines = {}
+            for commission in commissions:
+                invoice_lines.setdefault(commission.order_customer_id, list())
+                invoice_lines[commission.order_customer_id].append(commission)
+            for customer, commissions in invoice_lines.iteritems():
+                self.pool.get('account.invoice.line').create(cursor, user_id, {
+                    'name' : customer.name,
+                    'invoice_id' : invoice_id,
+                    'product_id' : product.id,
+                    'account_id' : product.property_account_income.id,
+                    'price_unit' : sum([commission.commission_amount for commission in commissions]),
+                    'quantity' : 1,
+                    'origin' : commission.order_id.name,
+                }, context=context)
+
+        view_id = self.pool.get('ir.ui.view').read(cursor, user_id,
+            self.pool.get('ir.ui.view').search(
+                cursor, user_id, [('name', '=', 'account.invoice.form')] ,context=context),
+            context=context)[0]['id']
+        view_xmlid = self.pool.get('ir.ui.view').get_xml_id(cursor, user_id, [view_id], context=context)[view_id]
 
         return {
             'name' : 'Commissions Invoices',
             'type' : 'ir.actions.act_window',
             'view_type' : 'form',
             'view_mode' : 'tree,form',
+            'context' : {'form_view_ref' : view_xmlid},
             'res_model' : 'account.invoice',
-            'res_id' : suppliers_invoices.values(),
+            'domain' : [('id', 'in', suppliers_invoices.values())],
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
         }
 
 CreateCommissionsInvoice()
